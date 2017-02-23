4ebeccc216e0ff0c28ec908a684723c842fad2da
Mello007 <sindikat008@gmail.com>
2017-02-23 21:54:58 +0300

    changed methods, add PreparedStatement to all methods

--- src/service/src/main/java/ru/structure/service/StructureService.java
+++ src/service/src/main/java/ru/structure/service/StructureService.java
@@ -2,10 +2,8 @@ package ru.structure.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.postgresql.util.PGobject;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
@@ -18,7 +16,6 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -44,7 +41,6 @@ public class StructureService {
     }
 
     public void create(String json) throws IOException, SQLException {
-        PGobject jsonObject = createPgObject(json);
         String sql = "insert into structures (data) values (?::JSONB)";
 //        jdbcTemplateObject.update(sql, jsonObject);
         KeyHolder keyHolder = new GeneratedKeyHolder();
@@ -62,14 +58,11 @@ public class StructureService {
         String SQL = "select * from structures where id = ?";
         Structure structure = jdbcTemplateObject.queryForObject(SQL,
                 new Object[]{id}, new StructureMapper());
-        String d =  structure.getData().replace("\\", "");
-        structure.setData(d);
         return structure;
     }
 
-    public void delete(Integer id){
+    public void delete(Integer id) {
         String sql = "delete from structures where id = ?";
-        jdbcTemplateObject.update(sql, id);
         jdbcTemplateObject.query(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -82,7 +75,6 @@ public class StructureService {
 
 
     public void updateStructure(Long id, String json) throws SQLException {
-        PGobject jsonObject = createPgObject(json);
         String sql = "update structures set data = ?::JSONB where id = ?";
 //        jdbcTemplateObject.update(sql, jsonObject, id);
         KeyHolder keyHolder = new GeneratedKeyHolder();
@@ -103,14 +95,22 @@ public class StructureService {
         }, keyHolder);
     }
 
-    public List<Record> getRecordsByStructureId(Long id){
+    public List<Record> getRecordsByStructureId(Long id) {
         String sql = "SELECT * FROM records INNER JOIN structures ON records.structure_id = structures.id WHERE structure_id = ?";
-        List <Record> records = jdbcTemplateObject.query(sql, new Object[]{id}, new RecordMapper());
+        // List <Record> records = jdbcTemplateObject.query(sql, new Object[]{id}, new RecordMapper());
+        List<Record> records = jdbcTemplateObject.query(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setObject(1, id);
+                return ps;
+            }
+        }, new RecordMapper());
         return records;
     }
 
     public void createRecordByStructure(Long id, String json) throws SQLException {
-        String sql = "insert into records (structure_id, data) values (?, ?::JSONB)" ;
+        String sql = "insert into records (structure_id, data) values (?, ?::JSONB)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplateObject.update(new PreparedStatementCreator() {
             @Override
@@ -149,26 +149,22 @@ public class StructureService {
         }, keyHolder);
     }
 
-    public void deleteRecordByStructureId(Long recordId){
-        String SQL = "delete from records where id = ?";
-        jdbcTemplateObject.update(SQL, recordId);
+    public void deleteRecordByStructureId(Long recordId) {
+        String sql = "delete from records where id = ?";
+        jdbcTemplateObject.query(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ps.setObject(1, recordId);
+                return ps;
+            }
+        }, new RecordMapper());
     }
 
     public Record getRecordById(Long id) {
         String SQL = "select * from records where id = ?";
         Record record = jdbcTemplateObject.queryForObject(SQL,
                 new Object[]{id}, new RecordMapper());
-
-
-        record.setData(record.getData().replace("\\", "").replace("n", ""));
         return record;
     }
-
-    private PGobject createPgObject(String json) throws SQLException {
-        PGobject jsonObject = new PGobject();
-        jsonObject.setType("json");
-        jsonObject.setValue(json);
-        return jsonObject;
-    }
-
 }
